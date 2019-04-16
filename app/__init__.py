d9e4afde73fd6b51651561df13afcf94ad25be92
snkoski <shawnnkoski@gmail.com>
2019-04-16 19:47:37 -0400

    Added token Delete route. Created errors blueprint. Set up errors handlers to convert 404 and 500 errors to json.

--- app/__init__.py
+++ app/__init__.py
@@ -21,6 +21,9 @@ def create_app(config_class=Config):
     ma.init_app(app)
     login.init_app(app)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
     from app.main import bp as main_bp
     app.register_blueprint(main.bp)
 
